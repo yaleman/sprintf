7854d1e7d0f6910a79b6761a2886c07219fb39f0
James Hodgkinson <james@terminaloutcomes.com>
2022-01-29 01:07:45 +1000

    adding more type annotation things

--- sprintf/__init__.py
+++ sprintf/__init__.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import json
 import os.path
 from pathlib import Path
-from typing import Optional
+from typing import Dict, Optional
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, HTMLResponse
@@ -16,14 +16,14 @@ class UserQuery(BaseModel):
     # datestring: Optional[str] = None
     formatstring: str
 
+class ErrorResult(BaseModel):
+    """ error message """
+    error: str
+
+
 app = FastAPI()
 
-
-def error_result(msg: str):
-    """ returns a dict """
-    return { "error" : msg }
-
-def parse_user_input(query: UserQuery):
+def parse_user_input(query: UserQuery) -> Dict[str, str]:
     """ does the handling bit """
     try:
         date_object = datetime.now(tz=timezone.utc)
@@ -32,31 +32,31 @@ def parse_user_input(query: UserQuery):
         }
     # pylint: disable=broad-except
     except Exception as error:
-        return error_result(f"Failed to parse: error {error}")
+        return ErrorResult(error=f"Failed to parse: error {error}").dict()
 
 @app.post("/parse")
-async def parse(query: UserQuery):
+async def parse(query: UserQuery) -> Dict[str, str]:
     """ parse a request then responds """
     print(json.dumps(query, default=str))
     return parse_user_input(query)
 
 @app.get("/chibi.js")
-async def chibijs():
+async def chibijs() -> FileResponse:
     """ return the chibi file """
     return FileResponse(f"{os.path.dirname(__file__)}/chibi-min.js")
 
 @app.get("/favicon.png")
-async def favicon():
+async def favicon() -> FileResponse:
     """ return the favicon file """
     return FileResponse(f"{os.path.dirname(__file__)}/favicon.png")
 
 @app.get("/")
-async def root(f: Optional[str] = None): # pylint: disable=invalid-name
+async def root(f: Optional[str] = None) -> HTMLResponse: # pylint: disable=invalid-name
     """ homepage """
     indexfile = Path(f"{os.path.dirname(__file__)}/index.html")
     if not indexfile.exists():
         print("Couldn't find index file!")
-        return None
+        return HTMLResponse(content="File Not Found", status_code=404)
     content = indexfile.read_text(encoding="utf8")
 
     inputval = "%Y-%m-%d"
